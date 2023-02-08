e1c40ade1193b6fc4e5c5fb510d25542b4932f74
CarlieMartece <carliemartece@gmail.com>
2023-02-08 20:44:10 +0000

    remove self_ref from collage close ups

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -104,7 +104,8 @@ exports.getArtBy3Words = (req, res, next) => {
         collage[stockId] = art[0];
         collage[stockId].quote = "";
         collage[stockId].book_title = "";
-        
+        collage[stockId].self_ref = "TBC";
+
       });
       const idRef = createRef(artIds, "stock_id", "art_id");
       const wordRef = createRef(artIds, "stock_id", "three_word_description");
